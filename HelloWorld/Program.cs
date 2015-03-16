b20bff2820b057e1e8240f3fa1b038a30545f3d9
phani Kurada <pkurada@hotmail.com>
2015-03-16 13:05:12 -0400

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage("Hello World!"));
+        }
+
+        public static string CreateMessage(string message)
+        {
+            return message;
+        }
+
+       
+    }
+}
---
+++ HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateMessage()
+        {
+            Assert.AreEqual("Hello World!", Program.CreateMessage("Hello World!"));
+        }
+    }
+}
